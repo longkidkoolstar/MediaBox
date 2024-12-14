3a0d0c73345cb2b9a0fb6313ebee01cd920ccfab
longkidkoolstar <75497675+longkidkoolstar@users.noreply.github.com>
2024-12-13 19:28:46 -0500

    attempt to make it a webapp

---
+++ worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('media-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/css/styles.css',
+          '/js/favorites.js',
+          '/js/home.js',
+          // Add other assets you want to cache
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
