17aa7af2b77fe97581a46c73b3f297121506806e
longkidkoolstar <babykoolstar@gmail.com>
2025-01-04 18:21:28 -0500

    try to fix vercel error

--- worker.js
+++ worker.js
@@ -1,24 +1,24 @@
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('media-cache').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/css/styles.css',
-          '/js/favorites.js',
-          '/js/home.js',
-          '/js/settings.js',
-          '/js/search.js',
-          // Add other assets you want to cache
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
+  event.waitUntil(
+    caches.open('media-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/css/styles.css',
+        '/js/favorites.js',
+        '/js/home.js',
+        '/js/settings.js',
+        '/js/search.js',
+        // Add other assets you want to cache
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
